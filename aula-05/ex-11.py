9906478aac8bd8a436ab8bd74a3f8f417b7bfd19
Gabriela Sales <95177458+gabriela-gnsales@users.noreply.github.com>
2022-09-05 01:05:33 -0300

    Lista 5

--- aula-05/ex-11.py
+++ aula-05/ex-11.py
@@ -10,51 +10,59 @@ Todas essas listas deverão ser adicionadas, na ordem correta, em uma lista gera
 Uma vez criada e calculada toda a tabela, pergunte (em loop) para a usuária qual mês ela deseja consultar e mostre para ela quanto de juros serão pagos, quanto será amortizado e qual será o seu saldo devedor naquele mês. Caso ela digite um mês inválido, informe para ela que aquele mês não existe. Caso ela digite um mês negativo, encerre o programa.
 '''
 
-print('\033[1;33m*' * 28, 'TABELA PRICE', '*' * 28, '\033[m')
+# print('\033[1;33m{:*^80}\033[m'.format(' TABELA PRICE '))
 
+print('\033[1;33m=' * 75)
+print('{:^80}'.format('TABELA PRICE'))
+print('\033[1;33m=\033[m' * 75)
+
+# Solicitando os dados de entrada
 valor_emprestimo = float(input('Informe o valor do empréstimo: '))
 taxa_juros = float(input('Informe o valor da taxa de juros (em %) do empréstimo: ')) / 100
 tempo_pagamento = int(input('Informe o tempo (em meses) para pagamento: '))  # número de parcelas
 
+# Calculando o valor fixo a ser pago por mês (prestação mensal)
 n = (1 + taxa_juros)**tempo_pagamento
-prestacao = valor_emprestimo * ((n * taxa_juros) / (n - 1))  # valor fixo a ser pago por mês
+prestacao = valor_emprestimo * ((n * taxa_juros) / (n - 1))
 
-print('\033[1;33m-\033[m' * 70)
-
-# print('\033[1;32mMês\033[m | \033[1;32mPrestação\033[m     | \033[1;32mJuros\033[m         | \033[1;32mAmortização\033[m   | \033[1;32mSaldo devedor\033[m')
-
+# Criando a lista com todos os valores mensais referentes ao empréstimo
+emprestimo_total = list()
 saldo_devedor = valor_emprestimo
-
-lista_juros = []
-lista_amortizacao = []
-lista_saldo_devedor = []
-lista = []
-
-for i in range(tempo_pagamento):
+for mes in range(tempo_pagamento):
 
     juros = saldo_devedor * taxa_juros
-    lista_juros.append(juros)
-
     amortizacao = prestacao - juros
-    lista_amortizacao.append(amortizacao)
-
     saldo_devedor -= amortizacao
-    lista_saldo_devedor.append(saldo_devedor)
 
-    for j in range(5):
-        lista[0] = lista.append(prestacao)
-        lista[1] = lista.append(juros)
-        lista[2] = lista.append(amortizacao)
-        lista[3] = lista.append(saldo_devedor)
+    emprestimo_mensal = list()
+    emprestimo_mensal.append(prestacao)
+    emprestimo_mensal.append(juros)
+    emprestimo_mensal.append(amortizacao)
+    emprestimo_mensal.append(saldo_devedor)
 
-    # print(f'{(i+1):^3} | R$ {prestacao:^10.2f} | R$ {juros:^10.2f} | R$ {amortizacao:^10.2f} | R$ {saldo_devedor:^10.2f}')
+    emprestimo_total.append(emprestimo_mensal)
 
-print(f'Lista:', lista)
+print('\033[1;33m{:-^75}\033[m'.format(' Início da consulta '))
 
-# print(f'Lista juros:', lista_juros)
-# print('Lista amortizacao:', lista_amortizacao)
-# print('Lista saldo devedor:', lista_saldo_devedor)
+# Lendo a consulta da usuária
+while True:
+    mes = int(input('Qual mês deseja consultar [para encerrar digite um valor negativo]? '))
 
+    while mes not in range(1, tempo_pagamento + 1) and mes >= 0:
+        print('\033[0;31mEsse mês não existe. Responda novamente:\033[m')
+        mes = int(input('Qual mês deseja consultar [para encerrar digite um valor negativo]? '))
 
+    if mes < 0:
+        break
 
-print('\033[1;33m-\033[m' * 70)
+    for m in range(len(emprestimo_total)):
+        if m == (mes - 1):
+            print('\033[1;32m-\033[m' * 28)
+            print('\033[1;32m{:^30}\033[m'.format(f'MÊS {mes}'))
+            print('{:14} {:.<2} {:>8.2f}'.format('Prestação:', 'R$', emprestimo_total[m][0]))
+            print('{:14} {:<2} {:>8.2f}'.format('Juros:', 'R$', emprestimo_total[m][1]))
+            print('{:14} {:<2} {:>8.2f}'.format('Amortização:', 'R$', emprestimo_total[m][2]))
+            print('{:14} {:<2} {:>8.2f}'.format('Saldo devedor:', 'R$', emprestimo_total[m][3]))
+            print('\033[1;32m-\033[m' * 28)
+
+print('\033[1;33m{:-^75}\033[m'.format(' Fim da consulta '))
